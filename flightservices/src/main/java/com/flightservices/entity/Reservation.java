750d71063e87c2a706c736b896792f9af373dd35
netajik <kancharapunetaji@gmail.com>
2021-01-17 11:59:49 -0500

    entities created for flightservices and frontend project initiated

---
+++ flightservices/src/main/java/com/flightservices/entity/Reservation.java
@@ -0,0 +1,27 @@
+package com.flightservices.entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Reservation extends AbstractEntity {
+
+	private Boolean checkedIn;
+	private int numberOfBags;
+
+	public Boolean getCheckedIn() {
+		return checkedIn;
+	}
+
+	public void setCheckedIn(Boolean checkedIn) {
+		this.checkedIn = checkedIn;
+	}
+
+	public int getNumberOfBags() {
+		return numberOfBags;
+	}
+
+	public void setNumberOfBags(int numberOfBags) {
+		this.numberOfBags = numberOfBags;
+	}
+
+}
